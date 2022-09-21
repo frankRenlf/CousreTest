e30c0ac0a076c17d5549a8fe0085f27b44951292
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-21 10:44:01 +0800

    update

--- 48day/src/Day9/Main.java
+++ 48day/src/Day9/Main.java
@@ -1,5 +1,7 @@
 package Day9;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +16,37 @@ package Day9;
  */
 public class Main {
 
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt() + 1, m = sc.nextInt() + 1;
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < n; i++) {
+                dp[0][i] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                dp[i][0] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j] + dp[i][j - 1]);
+                }
+            }
+            System.out.println(dp[m - 1][n - 1]);
+        }
+    }
 
+    public int addAB(int A, int B) {
+        // write code here
+        if (B == 0) {
+            return A;
+        }
+        while (B != 0) {
+            int sum = A ^ B;
+            int carray = (A & B) << 1;
+            A = sum;
+            B = carray;
+        }
+        return A;
+    }
 }
