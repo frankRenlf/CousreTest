fe44dc06b80ca31e7366121b502b6b13c6b242b2
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-22 12:07:33 +0800

    Update Main.java

--- 48day/src/Day10/Main.java
+++ 48day/src/Day10/Main.java
@@ -18,9 +18,15 @@ import java.util.Scanner;
  * @Description :
  */
 public class Main {
-
-
     public static void main(String[] args) {
+        byte b1 = 1, b2 = 2, b3, b6;
+        final byte b4 = 4, b5 = 6;
+        b6 = b4 + b5;
+        b3 = (byte) (b1 + b2);
+        System.out.println(b3 + b6);
+    }
+
+    public static void main1(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String s = sc.nextLine();
