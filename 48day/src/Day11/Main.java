8f0cfca982ede7a0694201ae174754386edaa2cc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-23 10:45:34 +0800

    Create Main.java

---
+++ 48day/src/Day11/Main.java
@@ -0,0 +1,30 @@
+package Day11;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CousreTest
+ * @Package : Day11
+ * @createTime : 2022/9/23 10:42
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Main {
+    
+    public int getLCA(int a, int b) {
+        //每次都让较大的节点先找父节点,
+        // 直到 a和b相遇!就是公共节点!
+        while (a != b) {
+            if (a > b) {
+                a /= 2;
+            } else {
+                b /= 2;
+            }
+        }
+        return a;
+    }
+
+}
