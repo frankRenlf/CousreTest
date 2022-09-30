adaa9839c5743c88160723746e7f51a137086ced
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-30 22:31:55 +0800

    1

--- 48day/src/Day18/Main.java
+++ 48day/src/Day18/Main.java
@@ -1,5 +1,7 @@
 package Day18;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,4 +15,52 @@ package Day18;
  * @Description :
  */
 public class Main {
+
+    public static boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+            System.out.println(isMatch(s2, s1));
+        }
+    }
+
+    public static void main1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int[] dp = new int[n + 1];
+            dp[1] = dp[2] = 1;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 2] + dp[i - 1];
+            }
+            System.out.println(dp[n]);
+        }
+    }
+
 }
