6cb9de390e1d8e26edaca6ebfec6b591e31c4a6d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-02 09:15:26 +0800

    Update Main.java

--- 48day/src/Day18/Main.java
+++ 48day/src/Day18/Main.java
@@ -1,5 +1,7 @@
 package Day18;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 
@@ -81,6 +83,50 @@ public class Main {
             }
         }
         return dp[m][n];
+    }
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        // Build the graph
+        int mat[][] = new int[n][n];
+        for (int flight[] : flights) {
+            mat[flight[0]][flight[1]] = flight[2];
+        }
+
+        // min heap: {(vertex, cost, stops), ...}
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
+
+        // costs[i]: min cost from src to vertex i
+        // stops[i]: number of stops of the corresponding cheapest cost for vertex i
+        int costs[] = new int[n];
+        int stops[] = new int[n];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+
+        // Dijkstra Algorithm within k
+        minHeap.offer(new int[]{src, 0, k});
+        while (!minHeap.isEmpty()) {
+            int elem[] = minHeap.poll();
+            int vertex = elem[0], cost = elem[1], stop = elem[2];
+
+            if (vertex == dst) {
+                return cost;
+            } else if (stop < 0) {
+                continue;
+            }
+
+            for (int i = 0; i < n; ++i) {
+                if (mat[vertex][i] > 0) {
+                    int costI = costs[i], costVI = mat[vertex][i];
+                    if (cost + costVI < costI) {
+                        minHeap.offer(new int[]{i, costVI + cost, stop - 1});
+                        costs[i] = costVI + cost;
+                        stops[i] = stop - 1;
+                    } else if (stops[i] < stop - 1) {
+                        minHeap.offer(new int[]{i, costVI + cost, stop - 1});
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
 
