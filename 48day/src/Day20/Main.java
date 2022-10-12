2165ef579b788a194282d1e82f487692dbbf78ea
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-12 11:55:09 +0800

    Create Main.java

---
+++ 48day/src/Day20/Main.java
@@ -0,0 +1,28 @@
+package Day20;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CousreTest
+ * @Package : Day20
+ * @createTime : 2022/10/12 11:52
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            StringBuilder sb = new StringBuilder(s);
+            System.out.println(sb.reverse().toString());
+        }
+    }
+
+}
