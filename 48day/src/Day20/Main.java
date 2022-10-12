9c725e9eb196e52b4e47e645c49e7ba4948632d5
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-12 12:01:48 +0800

    update

--- 48day/src/Day20/Main.java
+++ 48day/src/Day20/Main.java
@@ -19,6 +19,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+            if (s1.length() <= s2.length()) find(s1, s2);
+            else find(s2, s1);
+        }
+    }
+
+    private static void find(String s1, String s2) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int[][] dp = new int[l1 + 1][l2 + 1];
+        int maxLen = 0;
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                }
+            }
+        }
+        System.out.println(maxLen);
+    }
+
+    public static void main1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
             String s = sc.nextLine();
             StringBuilder sb = new StringBuilder(s);
             System.out.println(sb.reverse().toString());
