0806894e85e9bcb884772b8dfbdc75776d92af2c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-19 11:04:27 +0800

    create day 27

---
+++ 48day/src/Day27/Main.java
@@ -0,0 +1,16 @@
+package Day27;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CousreTest
+ * @Package : Day27
+ * @createTime : 2022/10/19 11:03
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Main {
+}
