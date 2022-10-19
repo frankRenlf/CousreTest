031ab7cf012884bef6c1409947e8006636ba23c8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-19 11:31:49 +0800

    update

--- 48day/src/Day27/Main.java
+++ 48day/src/Day27/Main.java
@@ -1,5 +1,7 @@
 package Day27;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,4 +15,30 @@ package Day27;
  * @Description :
  */
 public class Main {
+    public int Add(int num1, int num2) {
+        while (num2 != 0) {
+            int tmp = num1 ^ num2;
+            num2 = (num1 & num2) << 1;
+            num1 = tmp;
+            // 101 100
+            // 001 1000
+            // 1001
+        }
+        return num1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            double a = in.nextDouble();
+            double b = in.nextDouble();
+            double c = in.nextDouble();
+            if(a+b>c && a+c>b && b+c>a){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        }
+    }
+
 }
