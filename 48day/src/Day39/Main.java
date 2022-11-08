245d0bfc3efb779d46e1b00e7989b07dbea2ba92
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-08 22:23:03 +0800

    update

--- 48day/src/Day39/Main.java
+++ 48day/src/Day39/Main.java
@@ -56,8 +56,17 @@ public class Main {
         while (sc.hasNext()) {
             char[] s1 = sc.next().toCharArray(), s2 = sc.next().toCharArray();
             int m = s1.length, n = s2.length;
-            int[][] dp = new int[m][n];
-            for (int)
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (s1[i - 1] == s2[j - 1]) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            System.out.println(dp[m][n]);
         }
     }
 
