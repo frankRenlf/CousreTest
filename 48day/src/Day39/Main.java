d94fa743ca5e923ac6f7fcb2fdea55ab42c1b0aa
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-08 21:26:54 +0800

    Create Main.java

---
+++ 48day/src/Day39/Main.java
@@ -0,0 +1,16 @@
+package Day39;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CousreTest
+ * @Package : Day39
+ * @createTime : 2022/11/8 21:25
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Main {
+}
