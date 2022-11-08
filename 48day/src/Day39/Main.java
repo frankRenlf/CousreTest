d77546304ac87080473d5eeb0fe220536bd5be7b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-08 21:34:01 +0800

    update

--- 48day/src/Day39/Main.java
+++ 48day/src/Day39/Main.java
@@ -1,5 +1,7 @@
 package Day39;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,4 +15,50 @@ package Day39;
  * @Description :
  */
 public class Main {
+
+    private static int process(String str1, String str2, int len1, int len2) {
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+        long res = 0;
+        for (int i = len1; i <= len2; i++) {
+            char a = ch1[0];
+            char b = ch2[0];
+            res += (long) Math.pow(26, i - 1) * (b - a);
+            long suma = 0;
+            long sumb = 0;
+            for (int j = 1; j < ch1.length; j++)// 找到比ch1剩余字符串小的字符串个数
+            {
+                suma = suma + (ch1[j] - 'a') * (long) Math.pow(26, i - 1 - j);
+            }
+            for (int j = 1; j < ch2.length; j++)// 找到比ch2剩余字符串小的字符串个数
+            {
+                sumb = sumb + (ch2[j] - 'a') * (long) Math.pow(26, i - 1 - j);
+            }
+            res = res + sumb - suma;
+        }
+        res--;
+        res = res % 1000007;
+        return (int) res;
+    }
+
+    public static void main1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s1 = sc.next(), s2 = sc.next();
+            int len1 = sc.nextInt(), len2 = sc.nextInt();
+            int ret = process(s1, s2, len1, len2);
+            System.out.println(ret);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            char[] s1 = sc.next().toCharArray(), s2 = sc.next().toCharArray();
+            int m = s1.length, n = s2.length;
+            int[][] dp = new int[m][n];
+            for (int)
+        }
+    }
+
 }
