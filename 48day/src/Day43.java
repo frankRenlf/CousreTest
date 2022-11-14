39b0476f916f5852246430fcdb25043535f06e17
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-14 08:43:31 +0800

    initiate new day work

---
+++ 48day/src/Day43.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : CousreTest
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/11/14 8:42
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class Day43 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+        }
+    }
+
+}
